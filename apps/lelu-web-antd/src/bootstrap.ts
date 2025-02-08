78af40ebe793d7ffe12d8e99f55a51d18260013a
Ethan <747602467@qq.com>
2025-02-08 11:49:10 +0800

    插件扫描

--- apps/lelu-web-antd/src/bootstrap.ts
+++ apps/lelu-web-antd/src/bootstrap.ts
@@ -1,26 +1,30 @@
-import {createApp} from 'vue';
+import { createApp } from "vue";
 
-import {registerAccessDirective} from '@vben/access';
-import {initStores} from '@vben/stores';
-import '@vben/styles';
-import '@vben/styles/antd';
-import fastCrud from './crudSettings'
-import {setupI18n} from '#/locales';
-import Antd from 'ant-design-vue';
+import { registerAccessDirective } from "@vben/access";
+import { initStores } from "@vben/stores";
+import "@vben/styles";
+import "@vben/styles/antd";
+import fastCrud from "./crudSettings";
+import { setupI18n } from "#/locales";
+import Antd from "ant-design-vue";
 
-import {initComponentAdapter} from './adapter/component';
-import App from './app.vue';
-import {router} from './router';
-import {Icon} from '@iconify/vue';
-import {registerIcons} from './iconSettings'
+import { initComponentAdapter } from "./adapter/component";
+import App from "./app.vue";
+import { router } from "./router";
+import { Icon } from "@iconify/vue";
+import { registerIcons } from "./iconSettings";
+// 自动注册插件
+import { scanAndInstallPlugins } from "#/views/plugins";
 
 async function bootstrap(namespace: string) {
   // 初始化组件适配器
   await initComponentAdapter();
 
   const app = createApp(App);
+  // 插件扫描
+  scanAndInstallPlugins(app);
   // 注册全局的 Icon 组件
-  app.component('Icon', Icon);
+  app.component("Icon", Icon);
   // 调用函数来注册本地图标集合
   await registerIcons();
 
@@ -28,7 +32,7 @@ async function bootstrap(namespace: string) {
   await setupI18n(app);
 
   // 配置 pinia-tore
-  await initStores(app, {namespace});
+  await initStores(app, { namespace });
 
   // 安装权限指令
   registerAccessDirective(app);
@@ -39,7 +43,7 @@ async function bootstrap(namespace: string) {
   app.use(fastCrud);
   app.use(Antd);
 
-  app.mount('#app');
+  app.mount("#app");
 }
 
-export {bootstrap};
+export { bootstrap };
--- apps/lelu-web-antd/src/views/plugins/index.ts
+++ apps/lelu-web-antd/src/views/plugins/index.ts
@@ -1,18 +1,23 @@
-import { defineAsyncComponent } from 'vue';
-import type { AsyncComponentLoader } from 'vue';
-
 export let pluginsAll: any = [];
 // 扫描插件目录并注册插件
 export const scanAndInstallPlugins = (app: any) => {
-	const components = import.meta.glob('./**/*.vue');
-	const pluginNames = new Set();
-	// 遍历对象并注册异步组件
-	for (const [key, value] of Object.entries(components)) {
-		const name = key.slice(key.lastIndexOf('/') + 1, key.lastIndexOf('.'));
-		app.component(name, defineAsyncComponent(value as AsyncComponentLoader));
-		const pluginsName = key.match(/\/([^\/]*)\//)?.[1];
-		pluginNames.add(pluginsName);
-	}
-	pluginsAll = Array.from(pluginNames);
-	console.log('已发现插件：', pluginsAll);
+  const components = import.meta.glob("./*/index.ts"); // 查找 src 文件夹下的所有 index.ts 文件
+  const pluginNames = new Set();
+
+  // 遍历找到的 index.ts 文件
+  for (const [key, value] of Object.entries(components)) {
+
+    // 动态导入 index.ts 文件并执行其中的 install 方法
+    value().then((module: any) => {
+      if (module.default.install) {
+        app.use(module.default.install); // 如果有 install 方法，执行它
+      }
+    });
+
+    const pluginsName = key.match(/\/([^\/]*)\//)?.[1];
+    pluginNames.add(pluginsName);
+  }
+
+  const pluginsAll = Array.from(pluginNames);
+  console.log("已发现插件：", pluginsAll);
 };
